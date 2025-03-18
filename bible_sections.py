d84d0aaea2636d32a69d8b3b2e506a07359696c6
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-18 10:46:26 -0500

    Create separate bible_sections.py

---
+++ bible_sections.py
@@ -0,0 +1,51 @@
+old_testament = [
+    'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
+    'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings',
+    '1 Chronicles', '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther',
+    'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Songs',
+    'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel',
+    'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum',
+    'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi',
+    'Tobit', 'Judith', 'Wisdom', 'Sirach', 'Baruch'
+]
+
+pentateuch = [
+    "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"
+]
+
+historical_books = [
+    "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Tobit", "Judith", "Esther", "1 Maccabees", "2 Maccabees"
+]
+
+poetic_books = [
+    "Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Songs", "Wisdom", "Sirach"
+]
+
+prophetic_books = [
+    "Isaiah", "Jeremiah", "Lamentations", "Baruch", "Ezekiel", "Daniel", "Hosea", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi"
+]
+
+new_testament = [
+    'Matthew', 'Mark', 'Luke', 'John', 'Acts',
+    'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians',
+    'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians',
+    '1 Timothy', '2 Timothy', 'Titus', 'Philemon',
+    'Hebrews', 'James', '1 Peter', '2 Peter', '1 John', '2 John', '3 John',
+    'Jude', 'Revelation'
+]
+
+gospels = [
+    "Matthew", "Mark", "Luke", "John"
+]
+
+pauline_epistles = [
+    "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon"
+]
+
+general_epistles = [
+    "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude"
+]
+
+revelation = [
+    "Revelation"
+]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from bible_data_loader import load_bible_data
 from text_preprocessing import clean_text, preprocess_text
+import bible_sections
 
 # Create the Biblical Dataframe
 biblical_dataframe = load_bible_data()
@@ -31,58 +32,6 @@ for biblical_book, frequencies in biblical_book_frequencies.items():
 
     print("-" * (book_title_length))
 
-# Define Bible Sections:
-old_testament = [
-    'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
-    'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings',
-    '1 Chronicles', '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther',
-    'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Songs',
-    'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel',
-    'Hosea', 'Joel', 'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum',
-    'Habakkuk', 'Zephaniah', 'Haggai', 'Zechariah', 'Malachi',
-    'Tobit', 'Judith', 'Wisdom', 'Sirach', 'Baruch'
-]
-
-pentateuch = [
-    "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy"
-]
-
-historical_books = [
-    "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Tobit", "Judith", "Esther", "1 Maccabees", "2 Maccabees"
-]
-
-poetic_books = [
-    "Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Songs", "Wisdom", "Sirach"
-]
-
-prophetic_books = [
-    "Isaiah", "Jeremiah", "Lamentations", "Baruch", "Ezekiel", "Daniel", "Hosea", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi"
-]
-
-new_testament = [
-    'Matthew', 'Mark', 'Luke', 'John', 'Acts',
-    'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians',
-    'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians',
-    '1 Timothy', '2 Timothy', 'Titus', 'Philemon',
-    'Hebrews', 'James', '1 Peter', '2 Peter', '1 John', '2 John', '3 John',
-    'Jude', 'Revelation'
-]
-
-gospels = [
-    "Matthew", "Mark", "Luke", "John"
-]
-
-pauline_epistles = [
-    "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon"
-]
-
-general_epistles = [
-    "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude"
-]
-
-revelation = [
-    "Revelation"
-]
 
 # Analyze word frequencies based on section
 def get_section_frequencies(biblical_set):
@@ -90,10 +39,10 @@ def get_section_frequencies(biblical_set):
     cumulative_words = [word for sublist in section_dataframe["processed_words"] for word in sublist]
     return Counter(cumulative_words)
 
-new_testament_frequencies = get_section_frequencies(new_testament)
-gospels_frequencies = get_section_frequencies(gospels)
-general_epistles_frequencies = get_section_frequencies(general_epistles)
-pauline_epistles_frequencies = get_section_frequencies(pauline_epistles)
+new_testament_frequencies = get_section_frequencies(bible_sections.new_testament)
+gospels_frequencies = get_section_frequencies(bible_sections.gospels)
+general_epistles_frequencies = get_section_frequencies(bible_sections.general_epistles)
+pauline_epistles_frequencies = get_section_frequencies(bible_sections.pauline_epistles)
 
 section_title = "\n--- NEW TESTAMENT ---"
 section_title_length = len(section_title) - 1
